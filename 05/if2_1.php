d86425076d15a7583247ea5de5b49c795e4022d7
momoandmike <momoandmike@gmail.com>
2022-02-25 20:25:07 +0900

    制御構文2の課題修正

--- 05/if2_1.php
+++ 05/if2_1.php
@@ -7,10 +7,9 @@ echo "あなたの所持金は{$poket_money}円です。<br>";
 
 while ($poket_money >= 101) {
     $poket_money = $poket_money - $fund_raising;
-    if ($poket_money < 1000) {
-        echo "{$fund_raising}円募金しました。<br>";
-        echo "残り残高は{$poket_money}円です。<br>";
-    }
+
+    echo "{$fund_raising}円募金しました。<br>";
+    echo "残り残高は{$poket_money}円です。<br>";
 }
 if ($poket_money < $fund_raising)
     echo "あなたはこれ以上募金できません。";
